3895f4964b3d6d7254f5ea0ab409195a96c94558
Sanjana Swami <45294446+Sanju-6@users.noreply.github.com>
2019-08-24 03:00:52 -0300

    All filbert animations and movements work with this.

---
+++ Behaviour.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Behaviour : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private Rigidbody2D rb;
+
+    private bool facingRight = true;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    public int extraJumps;
+    public int extraJumpsValue;
+
+    //for transition
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        extraJumps = extraJumpsValue;
+        rb = GetComponent<Rigidbody2D>();    
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }
+        else if(facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+    }
+
+    void Update()
+    {
+        animator.SetFloat("Speed", Mathf.Abs(moveInput * speed));
+        if(isGrounded == true)
+        {
+            extraJumps = extraJumpsValue;
+            animator.SetBool("IsJumping", false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0)
+        {
+            animator.SetBool("IsJumping", true);
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if(Input.GetKeyDown(KeyCode.UpArrow) && extraJumps == 0 && isGrounded == true)
+        {
+            animator.SetBool("IsJumping", true);
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+}
