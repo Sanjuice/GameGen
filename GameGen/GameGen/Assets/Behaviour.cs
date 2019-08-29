7f5a7dd0425a4cde7d32c5d0dfc057dc8928860e
Sanjana Swami <sn696414@dal.ca>
2019-08-29 12:54:36 -0300

    GameGen full unity project

---
+++ GameGen/GameGen/Assets/Behaviour.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Behaviour : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+
+    private Rigidbody2D rb;
+
+    private bool facingRight = true;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    public int extraJumps;
+    public int extraJumpsValue;
+
+    [SerializeField] Transform spawnPoint;
+
+    //for transition
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        extraJumps = extraJumpsValue;
+        rb = GetComponent<Rigidbody2D>();    
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+        moveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }
+        else if(facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+    }
+
+    void Update()
+    {
+        animator.SetFloat("Speed", Mathf.Abs(moveInput * speed));
+        if(isGrounded == true)
+        {
+            extraJumps = extraJumpsValue;
+            animator.SetBool("IsJumping", false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.UpArrow) && extraJumps > 0)
+        {
+            animator.SetBool("IsJumping", true);
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if(Input.GetKeyDown(KeyCode.UpArrow) && extraJumps == 0 && isGrounded == true)
+        {
+            animator.SetBool("IsJumping", true);
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("bone"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+}
---
+++ GameGen/GameGen/Assets/Characters/killPlayah.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class killPlayah : MonoBehaviour
+{
+    [SerializeField] Transform spawnPoint;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.transform.CompareTag("Player"))
+        {
+            col.transform.position = spawnPoint.position;
+        }
+    }
+}
